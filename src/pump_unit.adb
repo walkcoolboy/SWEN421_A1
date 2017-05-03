d7307092c9acf8390ace3a97a71b28ebf368419f
Li Li <walkcoolboyli@gmail.com>
2017-05-04 08:37:56 +1200

    finishing pump unit body2

--- src/pump_unit.adb
+++ src/pump_unit.adb
@@ -4,14 +4,23 @@ with Pump; use Pump;
 
 package body Pump_unit is
 
-   procedure Lift_Nozzle (curr_fuel: in fuel_type) is
+   procedure setFuelVolume (f: in price) is
+   begin
+      fuel_needed := f;
+   end setFuelVolume;
+
+   procedure liftNozzle (curr_fuel: in fuel_type) is
    begin
       enterReadyState(curr_pump(curr_fuel));
    end Lift_Nozzle;
 
-   procedure Replace_Nozzle (curr_fuel: in fuel_type) is
+   procedure replaceNozzle (curr_fuel: in fuel_type) is
    begin
-      setCradle(curr_pump(curr_fuel));
+      enterWaitingState(curr_pump(curr_fuel));
    end Replace_Nozzle;
+
+   procedure requestPumping (curr_fuel: in fuel_type, f: in price) is
+   begin
+      if (f <=
 
 end Pump_Unit;
